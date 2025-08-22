ee0b20b61ada5f1a1ace3426576e786012dba103
kidus <kidusefrem2@gmail.com>
2025-08-22 16:21:49 +0300

    ahandle multiple socket pings

--- app/main.py
+++ app/main.py
@@ -10,7 +10,15 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     # server_socket.accept() # wait for client
     connection, addr = server_socket.accept()
-    connection.sendall(b"+PONG\r\n")
-    
+    while connection:
+        data = connection.recv(1024)
+        message = data.decode().split('\n')
+        for m in message:
+            connection.sendall(b"+PONG\r\n")
+    # with socket.create_connection(("localhost", 6379)) as server:
+    #     client, addr  = server.accept()
+    #     client.sendall(b"+PONG\r\n")
+    # with socket.creat
+
 if __name__ == "__main__":
     main()
