004542e627a6f9fa9c22fd5b25b064f481b1dc25
kidus <kidusefrem2@gmail.com>
2025-08-22 15:22:32 +0300

    test

--- app/main.py
+++ app/main.py
@@ -7,9 +7,14 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    # server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    # server_socket.accept() # wait for client
+    # connection = server_socket.accept()
+    # connection.sendall()
+    with socket.create_connection(("localhost", 6379)) as server:
+        client, addr  = server.accept()
+        client.sendall(b"+PONG\r\n")
+    # with socket.creat
 
 if __name__ == "__main__":
     main()
