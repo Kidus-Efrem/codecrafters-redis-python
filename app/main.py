150c5b911cf355c8fa0610e3bb1c40a70bd4873f
kidus <kidusefrem2@gmail.com>
2025-08-24 05:20:11 +0300

    test multiple pings

--- app/main.py
+++ app/main.py
@@ -7,18 +7,15 @@ def main():
 
     # Uncomment this to pass the first stage
     #
+    # client_socket = socket.
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    # server_socket.accept() # wait for client
-    connection, addr = server_socket.accept()
+    client_socket, _ = server_socket.accept()  # wait for client
     while True:
-        data = connection.recv(1024)
-        message = data.decode().strip()
-        if message.upper== "PING" :
-            connection.sendall(b"+PONG\r\n")
-    # with socket.create_connection(("localhost", 6379)) as server:
-    #     client, addr  = server.accept()
-    #     client.sendall(b"+PONG\r\n")
-    # with socket.creat
+        request: bytes = client_socket.recv(512)
+        data: str = request.decode()
 
+        # print(data)
+        if "ping" in data.lower():
+            client_socket.send("+PONG\r\n".encode())
 if __name__ == "__main__":
     main()
