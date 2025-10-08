df4d2e8ce75c2a9a572faa218698dcc545ad97a0
kidus <kidusefrem2@gmail.com>
2025-10-08 13:37:03 +0300

    test response to echo using resp

--- app/main.py
+++ app/main.py
@@ -7,7 +7,29 @@ async def handle_command(reader: asyncio.StreamReader, writer: asyncio.StreamWri
         chunk = await reader.read(BUF_SIZE)
         if not chunk:
             break
-        writer.write(b"+PONG\r\n")
+        i = 0
+        if chunk[i] == ord('*'):
+            i+=1
+            j = chunk.find('\r\n', i)
+            arrlen = int(chunk[i:j])
+        i = j+2
+        elements = []
+        for _ in range(arrlen):
+            if chunk[i] == ord('$'):
+                i+=1
+                j = chunk.find('\r\n', i)
+                wlen = int(chunk[i:j])
+                i = j+2
+                element = chunk[i:i+wlen]
+                i+= wlen+2
+                elements.append(element.decode())
+        print(elements)
+        if element[0].lower() == 'echo':
+            writer.write(b''+elements[1]+'\r\n')
+        if element[0].lower() == 'ping':
+            writer.write(b''+elements[1]+'\r\n')
+
+        # writer.write(b"+PONG\r\n")
         await writer.drain()
     writer.close()
     await writer.wait_closed()
