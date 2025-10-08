88b6097e38451bf37a27d3e5c93dddbdfaf7ef02
kidus <kidusefrem2@gmail.com>
2025-10-08 11:45:20 +0300

    test multiple users by event loop

--- app/main.py
+++ app/main.py
@@ -1,17 +1,21 @@
-import socket
-import threading
+import asyncio
 
 BUF_SIZE = 4096
 
-def handle_command(client: socket.socket):
-	while chunk := client.recv(BUF_SIZE):
-		if chunk == b"":
-			break
-		client.sendall(b"+PONG\r\n")
-def main():
-	server_socket = socket.create_server(("localhost", 6379), reuse_port = True)
-	while True:
-		client_socket, client_addr = server_socket.accept()
-		threading.Thread(target= handle_command, args= (client_socket,)).start()
+async def handle_command(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+    while True:
+        chunk = await reader.read(BUF_SIZE)
+        if not chunk:
+            break
+        writer.write(b"+PONG\r\n")
+        await writer.drain()
+    writer.close()
+    await writer.wait_closed()
+
+async def main():
+    server = await asyncio.start_server(handle_command, "localhost", 6379)
+    async with server:
+        await server.serve_forever()
+
 if __name__ == "__main__":
-	main()
+    asyncio.run(main())
