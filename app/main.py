46232e98b1a03099af885004d90b9455acee906e
kidus <kidusefrem2@gmail.com>
2025-10-08 14:55:54 +0300

    test set and get commands in resp

--- app/main.py
+++ app/main.py
@@ -37,7 +37,7 @@ async def handle_command(reader: asyncio.StreamReader, writer: asyncio.StreamWri
             writer.write(b"$2\r\nOK\r\n")
         if elements[0].lower() =='get':
             if word:= elements[1] in d:
-                writer.write(b'$' + str(len(word).encode()+ b'\r\n'+word.encode() + b"\r\n"))
+                writer.write(b'$' + str(len(word)).encode()+ b'\r\n'+word.encode() + b"\r\n")
             else:
                 writer.write(b"$-1\r\n")
         await writer.drain()
