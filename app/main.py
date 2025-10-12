3ef1c98c89eb0442708385e3ea0df49df883d9bf
kidus <kidusefrem2@gmail.com>
2025-10-13 02:25:10 +0300

    test type cmnd

--- app/main.py
+++ app/main.py
@@ -173,6 +173,9 @@ async def handle_command(reader: asyncio.StreamReader, writer: asyncio.StreamWri
                             await writer.drain()
 
                 asyncio.create_task(unblock_after_timeout())
+        elif cmd == 'type':
+            key = elements[i]
+            writer.write(b'\r\n' + b'$' + len(type(lst[key])) + '\r\n' + type(lst[key]).encode()+ '\r\n')
 
         else:
             writer.write(b"-ERR unknown command\r\n")
