a9b3493b80aba12ad98c49c6611913206e9103d2
kidus <kidusefrem2@gmail.com>
2025-10-28 15:13:14 +0300

    test xread

--- app/main.py
+++ app/main.py
@@ -222,6 +222,8 @@ async def handle_command(reader: asyncio.StreamReader, writer: asyncio.StreamWri
                     # Partial auto-generation (timestamp provided, sequence auto)
                     if t in lastusedseq:
                         sequence = lastusedseq[t] + 1
+                    elif t == 0:
+	                    sequence =1
                     else:
                         sequence = 0
                     final_id = f"{t}-{sequence}"
