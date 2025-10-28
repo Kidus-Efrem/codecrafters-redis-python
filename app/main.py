7fa0c2246f7a57f444da19d395e2da6ba5124df2
kidus <kidusefrem2@gmail.com>
2025-10-28 16:00:25 +0300

    test incr for missing key

--- app/main.py
+++ app/main.py
@@ -76,7 +76,7 @@ async def handle_command(reader: asyncio.StreamReader, writer: asyncio.StreamWri
                 writer.write(b"$-1\r\n")
 
         # ---------------- RPUSH ----------------
-        elif cmd == 'rpush': 
+        elif cmd == 'rpush':
             key = elements[1]
             for v in elements[2:]:
                 lst[key].append(v)
@@ -451,7 +451,7 @@ async def handle_command(reader: asyncio.StreamReader, writer: asyncio.StreamWri
                     expiry = time.time() + int(elements[4]) / 1000
 
                 d[key] = [value, expiry]
-                writer.write(b"+OK\r\n")
+                writer.write(b"1\r\n")
 
             # d[key] = (value, expiry)
 
